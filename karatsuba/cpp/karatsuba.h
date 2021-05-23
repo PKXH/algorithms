8ba73a434eb0267170b7d6321c30f0b570f60a22
PKXH <1498959+PKXH@users.noreply.github.com>
2021-05-22 18:56:22 -0700

    added header declaration for karatsuba function

---
+++ karatsuba/cpp/karatsuba.h
@@ -0,0 +1,16 @@
+//
+// karatsuba.h
+//
+// Created by PKXH on 22 May 2021
+//
+// Declarations for Karatsuba multiplcation function
+//
+#ifndef __karatsuba_h
+#define __karatsuba_h
+
+#include "IntList.h"
+
+int_list_sp karatsuba(int_list_sp, int_list_sp y);
+
+#endif // __karatsuba_h 
+

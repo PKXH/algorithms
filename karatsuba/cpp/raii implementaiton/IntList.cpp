14557ea6f697cd082350b220913f4e3d5d84395d
PKXH <1498959+PKXH@users.noreply.github.com>
2022-04-27 12:34:04 -0700

    added large integer tests; added reminder to implement to_uint protection against >MAX_INT invocation

--- karatsuba/cpp/raii implementaiton/IntList.cpp
+++ karatsuba/cpp/raii implementaiton/IntList.cpp
@@ -965,6 +965,23 @@ BOOST_AUTO_TEST_CASE(intlist_addition_operator_tests)
         BOOST_CHECK( a + b == a_plus_b );
     }
 
+    {   //
+        // two long ones, same length w/ final carry
+        //
+        IntList a {9,4,5,6,3,7,8,3,5,7,4,1,0,4,3,5,6,7,2,9,0,8,5,1,8,4,3,9,0,6,4,3,4,7,5,3,2,7,9,0,1,2,0,4,3,6,2,8,1,0,9,8,5,8,1,2,6,3,2,5,7,4};
+        IntList b {7,3,4,7,2,5,0,2,3,7,1,6,3,5,4,7,3,2,6,7,3,7,8,9,4,3,6,9,2,9,1,2,0,4,7,2,8,8,9,6,8,4,7,1,5,3,5,3,6,3,6,3,8,2,0,9,2,3,3,1,2,4};
+        IntList a_plus_b {1,6,8,0,3,6,2,8,5,9,4,5,7,3,9,8,2,9,9,9,6,4,6,4,1,2,8,0,8,3,5,5,5,5,2,2,6,1,6,8,6,9,6,7,5,8,9,8,1,7,4,6,2,4,0,2,1,8,6,5,6,9,8};
+        BOOST_CHECK( a + b == a_plus_b );
+    }
+
+    {   //
+        // two long one, different lengths
+        //
+        IntList a {5,5,2,0,8,4,2,2,5,3,2,2,7,1,0,4,6,0,9,0,0,4,2,5,6,7,3,2,7,9,9,7,8,4,3,4,6,8,8,9,5,4,8,2,3,9,5,8,7,2,3,0,7,4};
+        IntList b {7,3,5,6,7,8,2,3,0,9,3,4,2,7,3,6,4,9,8,3,4,7,8,9,7,4,8,9,5,7,6,2,9,3,6,8,1,7,6,5,2,9,0,8,1,2,3,4,5,0,3,0,5,8,2,3,8,7,4,3};
+        IntList a_plus_b {7,3,5,6,7,8,7,8,3,0,1,8,4,9,8,9,7,2,5,4,5,2,5,0,6,4,9,3,8,3,3,0,2,6,4,8,1,5,4,9,6,3,7,7,0,1,8,9,3,2,7,0,1,6,9,6,1,8,1,7};
+        BOOST_CHECK( a + b == a_plus_b );
+    }
 
     {   //
         // double-checking random values with c++ math
@@ -1029,11 +1046,14 @@ BOOST_AUTO_TEST_CASE(intlist_subtraction_operator_tests)
 // Just scale out and add up powers of 10.
 //
 // *******************************************************************************
-
+//
 unsigned int IntList::to_uint()
 {
     unsigned int sum = 0;
     unsigned int ten_to_the_i = 1;
+
+// TODO: add max_int guard; can implement by creating an int_list of max_int
+// and doing that comparison first?
 
     // start with the least significant digit, and add up the
     // powers of 10. NOTE that we have to process index 0, and
